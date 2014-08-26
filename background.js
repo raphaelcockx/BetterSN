57ce0d7973bb3f9da8fcf01c64bc11d6c48e4439
Raphael Cockx <raphael@cockx.com>
2014-08-26 21:27:09 +0200

    Show the number of nights when hovering over a price

--- background.js
+++ background.js
@@ -1,8 +1,20 @@
 $(function(){
+
+	/* Remove the unnecessary 'Departure' and 'Return' labels in flexible search */
 	$('tr.inboundHead th:not(.faresCalendarLinkPrevOut)').each(function() {
 		$(this).html('<b>'+$(this).find('b').text()+'</b>');
 	})
 	$('th.outboundHead').each(function() {
 		$(this).html('<b>'+$(this).find('b').text()+'</b>');
 	})
+
+	/* Experimental */
+	$('table#faresCalendar td[onclick]').each(function() {
+		var dates = $(this).attr('onclick').split("'");
+		var outboundDate = moment(dates[1], 'DD/MM/YYYY');
+		var inboundDate = moment(dates[3], 'DD/MM/YYYY');
+		var nights = inboundDate.diff(outboundDate, 'days');
+		$(this).attr('title', nights+' nights');
+	})
+
 });
